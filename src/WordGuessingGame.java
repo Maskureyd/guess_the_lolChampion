8e2262a73dd0a3684661a4735cc0e78d8e859a16
Emre Kaya <emre9kaya@gmail.com>
2023-05-11 21:17:46 +0300

    Update WordGuessingGame.java
    //Oyuna puan sistemi eklendi.Her yanlış tahminde oyuncudan 10 puan azalıyor.
    // Puan 0'a düştüğünde ise oyuncuya son 3 hak veriliyor , ve oyuncu 3 tahminde de cevabı bulamazsa 50 puan ceza yiyor.

--- src/WordGuessingGame.java
+++ src/WordGuessingGame.java
@@ -21,17 +21,18 @@ public class WordGuessingGame {
 
         char[] guessedWord = new char[selectedWord.length()];
         for (int i = 0; i < selectedWord.length(); i++) {
-            guessedWord[i] = '_';
+            guessedWord[i] = '-';
         }
 
-        int attempts = 5;
         boolean wordGuessed = false;
+        int startingScore = 100;
+        int incorrectGuessPenalty = 10;
+        int totalScore = startingScore;
 
         System.out.println("League of Legends Şampiyon Tahmin oyununa hoşgeldiniz.");
 
         while (!wordGuessed) {
             System.out.println("Tahmin etmek için bir harf girin: ");
-
             String userInput = scanner.next();
 
             if (userInput.length() != 1) {
@@ -50,19 +51,61 @@ public class WordGuessingGame {
             }
 
             if (!letterFound) {
-                attempts--;
+                totalScore -= incorrectGuessPenalty;
+                System.out.println("Yanlış tahmin! -" + incorrectGuessPenalty + " puan. Toplam puanınız: " + totalScore);
+            } else {
+                System.out.println("Doğru harf tahmin ettiniz, devam edin ve cevabı bulmaya çalışın.");
             }
 
-            if(attempts>0) {
+            if (totalScore > 0) {
                 System.out.println("Şampiyon Adı : " + String.valueOf(guessedWord));
-                System.out.println("Kalan tahmin hakkı: " + attempts);
+                System.out.println("Toplam puanınız: " + totalScore);
             }
+
             if (String.valueOf(guessedWord).equals(selectedWord.toLowerCase())) {
                 System.out.println("Tebrikler! Şampiyonu doğru tahmin ettiniz.");
+                System.out.println("Kazandığınız puan: " + totalScore);
                 break;
-            } else if (attempts ==0) {
-                System.out.println("Harf tahmin hakkınızı doldurdunuz ve şampiyonu bulamadınız.Doğru cevap : "+ selectedWord);
-                break;
+            } else if (totalScore == 0) {
+                int remainingAttempts = 3;
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Bu turdan puan alamayacaksınız. Cevabı tahmin etmeniz için " + remainingAttempts + " hakkınız kaldı.");
+                    System.out.println("Şampiyon Adı : " + String.valueOf(guessedWord));
+                    System.out.println("Lütfen bir harf girin: ");
+                    userInput = scanner.next();
+
+                    if (userInput.length() != 1) {
+                        System.out.println("Lütfen sadece tek bir harf girin.");
+                        i--;
+                        remainingAttempts--;
+                        continue;
+                    }
+
+                    guessedLetter = Character.toLowerCase(userInput.charAt(0));
+                    letterFound = false;
+
+                    for (int j = 0; j < selectedWord.length(); j++) {
+                        if (Character.toLowerCase(selectedWord.charAt(j)) == guessedLetter) {
+                            guessedWord[j] = guessedLetter;
+                            letterFound = true;
+                        }
+                    }
+
+                    if (letterFound) {
+                        System.out.println("Doğru harf tahmin ettiniz, devam edin ve cevabı bulmaya çalışın.");
+                        System.out.println("Şampiyon Adı : " + String.valueOf(guessedWord));
+                        break;
+                    } else {
+                        remainingAttempts--;
+                        System.out.println("Yanlış tahmin! Kalan tahmin hakkınız: " + remainingAttempts);
+                    }
+                }
+
+                if (remainingAttempts == 0) {
+                    totalScore -= 50;
+                    System.out.println("Harf tahmin hakkınızı doldurdunuz ve şampiyonu bulamadınız. -50 puan. Toplam puanınız: " + totalScore);
+                    break;
+                }
             }
         }
     }
